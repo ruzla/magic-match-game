5fc243e0d5905ac5553b814f3701d0891f71da29
Russell <russelldoe@hotmail.co.uk>
2022-07-06 21:06:57 +0100

    -refactor from app to SingleCard -add SingleCard style

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import SingleCard from './components/SingleCard'
+
 import './App.css';
 
 const cardImages = [
@@ -33,10 +35,7 @@ function App() {
 
      <div className="card-grid">
       {cards.map(card => (
-        <div className="card" key={card.id}>
-          <img className="front" src={card.src} alt="card front" />
-          <img className="back" src="/img/cover.png" alt="card back" />
-        </div>
+        <SingleCard key={card.id} card={card} />
       ))}
      </div>
     </div>
