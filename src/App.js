87ebf0a1415642dc722b7e1333f56135b20d6d8d
Russell <russelldoe@hotmail.co.uk>
2022-07-07 20:51:52 +0100

    -handle card choices

--- src/App.js
+++ src/App.js
@@ -15,6 +15,8 @@ const cardImages = [
 function App() {
   const [cards, setCards] = useState([])
   const [turns, setTurns] = useState(0)
+  const [choiceOne, setChoiceOne] = useState(null)
+  const [choiceTwo, setChoiceTwo] = useState(null)
 
   // shuffle cards
   const shuffleCards = () => {
@@ -26,7 +28,10 @@ function App() {
       setTurns(0)
   }
 
-  console.log(cards, turns)
+  // handle a choice
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+  }
 
   return (
     <div className="App">
@@ -35,7 +40,11 @@ function App() {
 
      <div className="card-grid">
       {cards.map(card => (
-        <SingleCard key={card.id} card={card} />
+        <SingleCard 
+          key={card.id} 
+          card={card}
+          handleChoice={handleChoice} 
+        />
       ))}
      </div>
     </div>
